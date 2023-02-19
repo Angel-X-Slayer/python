9a36e4ecfe1032b436d0d26c7e464aac80f13652
Angel-X-Slayer <ishanmukherjee11@gmail.com>
2023-02-20 02:29:51 +0530

    commit

--- date_codestudio_6.py
+++ date_codestudio_6.py
@@ -1,12 +1,21 @@
 arr=list(map(int,input().split(" ")))
 high=max(arr)
-low=min(arr)
-sum=int((high-low)/2)(high+low)
+low=high
+for i in arr:
+    if i<low and i>0:
+        low=i
+# print(high,low)
+k1=(high-low+1)
+k2=(high+low)
+sum1=int((k1*k2)/2)
+# print(sum1)
 s=0
 for i in arr:
     if i<0:
         pass
     else:
         s=s+i
-op=sum-s
+op=sum1-s
 print(op)
+
+
