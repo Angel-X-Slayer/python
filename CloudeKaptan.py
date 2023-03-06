9da34619fd8f0fba0fa57a5843a5fa033374353b
Angel-X-Slayer <ishanmukherjee11@gmail.com>
2023-03-07 00:02:00 +0530

    commit

--- CloudeKaptan.py
+++ CloudeKaptan.py
@@ -1,4 +1,10 @@
-a=2.33
-b=2
-c=int(a//b)
-print(c)
+str = input("enter the string: ")
+k = (list(reversed(str)))
+for i in k:
+    if i == " ":
+        k.remove(i)
+for i in range(len(str)):
+    if str[i] == " ":
+        k.insert(i, " ")
+
+print("".join(k))
